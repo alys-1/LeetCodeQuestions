1ccf0b20f3b10add4b5b14c587b88c2bbe148342
Sakshi Khedekar <119278465+alys-1@users.noreply.github.com>
2024-11-25 17:29:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (63.52%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -10,18 +10,18 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        int l=0, h=n;
-        while(l<=h){
-            int mid = l+ (h-l)/2;
-            if(guess(mid)==0){
+        int low = 0, high = n;
+       
+        while(low <= high){
+             int mid = low + (high - low)/2;
+            if(guess(mid) == 0){
                 return mid;
             }else if(guess(mid) == -1){
-                h = mid-1;
+                high = mid -1;
             }else{
-                l= mid+1;
+                low = mid + 1;
             }
         }
         return 0;
-        
     }
 };
