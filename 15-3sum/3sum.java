9329df1b5e2b51bb3003f5823212b72ee2368a1a
Sakshi Khedekar <119278465+alys-1@users.noreply.github.com>
2024-11-25 11:49:27 +0530

    Time: 52 ms (25.12%) | Memory: 52.5 MB (17.74%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        int a,b;
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+        Set<List<Integer>> al = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            a = i+1;
+            b = nums.length - 1;
+            while( a < b){
+                if(nums[a] + nums[b] == -(nums[i])){
+                    List<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[a]);
+                    l.add(nums[b]);
+                    al.add(l);
+                    while(a < b && nums[a] == nums[a+1]){
+                        a++;
                     }
+                    while(a < b && nums[b] == nums[b-1]){
+                        b--;
+                    }
+                    a++;
+                    b--;
+                }else if(nums[a] + nums[b] > -(nums[i])){
+                    b--;
+                }else{
+                    a++;
                 }
             }
         }
-        return res;        
+        List<List<Integer>> l = new ArrayList<>();
+        l.addAll(al);
+        return l;
     }
 }
