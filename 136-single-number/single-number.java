04fcd91b24d21c74bac7e2d92be3edb6f06f9b31
Sakshi Khedekar <119278465+alys-1@users.noreply.github.com>
2025-01-17 23:44:23 +0530

    Time: 6 ms (31.37%) | Memory: 46.8 MB (5.03%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        for (int num : nums) {
-            result ^= num;  // Bitwise XOR operation
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
         }
-        return result;
+        return nums[nums.length - 1];
     }
 }
